229e2d1640a096631058d75522107c4a6b722a8f
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2020-08-16 16:34:38 -0700

    Create canvas for mouse movement preview

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Remove old mouse preview elements
 document.getElementById('mouse-preview')?.remove();
+
+let canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+
+canvas.id = 'mouse-preview';
+canvas.style = 'position: fixed; top: 0; left: 0; z-index: 2147483647; background-color: transparent; margin: 0; padding: 0; pointer-events: none; user-select: none;';
+
+document.body.appendChild(canvas);
