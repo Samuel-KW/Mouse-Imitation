1f83e405ae32a3e4c7436a917765e919129daaf1
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2020-08-16 16:35:40 -0700

    Handle browser resize

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Remove old mouse preview elements
 document.getElementById('mouse-preview')?.remove();
 
+// Create a new canvas for previewing movement
 let canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
 
@@ -8,3 +9,18 @@ canvas.id = 'mouse-preview';
 canvas.style = 'position: fixed; top: 0; left: 0; z-index: 2147483647; background-color: transparent; margin: 0; padding: 0; pointer-events: none; user-select: none;';
 
 document.body.appendChild(canvas);
+
+// Handle resizing the browser
+function handle_resize () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+handle_resize();
+window.addEventListener('resize', handle_resize);
+
+// Initialize basic variable
+let x = 0, y = 0,
+    mouse_moved = false,
+    movement_queue = [];
+
